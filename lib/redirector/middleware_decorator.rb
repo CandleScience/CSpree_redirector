357d3e7d5ff767d5be127174609d3f71eeeec4d8
David Harmon <david@candlescience.com>
2015-11-05 15:41:14 -0500

    Revert attempts to overwrite redirector gem

--- lib/redirector/middleware_decorator.rb
+++ lib/redirector/middleware_decorator.rb
@@ -3,7 +3,7 @@ require 'redirector'
 Redirector::Middleware::Responder.class_eval do
   def redirect_uri
     destination_uri.tap do |uri|
-      uri.scheme ||= Rails.env.development? ? 'http' : 'https'
+      uri.scheme ||= (Rails.env.staging? || Rails.env.production?) ? 'https' : 'http'
       uri.host   ||= request_host
       uri.port   ||= request_port if request_port.present?
       uri.query  ||= env['QUERY_STRING'] if Redirector.preserve_query
